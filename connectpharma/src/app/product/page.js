7c59e3182d5a56d73fe09ed0dbeadb4e553ad452
ibsharakrami <91590546+ibsharakrami@users.noreply.github.com>
2024-02-16 00:08:18 +0530

    jamz starter

---
+++ connectpharma/src/app/product/page.js
@@ -0,0 +1,12 @@
+export default function Product() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+
+      <h1 className="bg-black text-center text-lg">Product</h1>
+
+
+
+    </main>
+  );
+}
